ad5caaf3ca0043d25abdb605dc9776c976f769af
DotTorrent <pointtorrent@outlook.com>
2021-05-10 01:13:44 +0800

    Mod: 将武器跟随方法移入父节点，新增了武器的弧形移动，优化了操作手感
    1. 将武器跟随的相关变量和方法移动到Weapon节点内
    2. 修改武器在上下移动时的样式，从直上直下变成弧形移动（椭圆）
    3. 优化鼠标操控方向的逻辑，设定了一个操控范围
    4. 调节武器在y方向跟随人物的速度

--- Actors/Player/Weapon/Sword/Sword.gd
+++ Actors/Player/Weapon/Sword/Sword.gd
@@ -1,67 +1,12 @@
 extends Node2D
 
 func _ready():
+	owner=get_parent().owner
 	pass
 
 func _physics_process(delta):
-	_move()
+	get_parent().follow_player()
 	
-	if owner.owner.input_module.is_attack_just_pressed:
+	if owner.input_module.is_attack_just_pressed:
 		$AnimationPlayer.play("attack")
 	pass
-
-# 追随角色的该武器(self)是否正在换边过程中
-var on_changing_side : = Vector2(false, false)
-
-func _move() :
-	# 该武器(self)位置和角色位置的目标偏移量
-	var target_offset : Vector2
-	# 当前鼠标位置和角色位置的偏移量
-	var mouse_offset_from_chara : = Vector2(owner.owner.input_module.mouse_global_position -  owner.owner.character.global_position)
-	# 当前该武器(self)位置和角色位置的偏移量
-	var weapon_offset_from_chara : = Vector2(global_position - owner.owner.character.global_position)
-
-	if(mouse_offset_from_chara.x > 0) :
-		scale.x = 1
-		target_offset = Vector2(15, 0)
-		
-		# 如果鼠标、该武器(self)并不在角色所在位置的左右两边，但是该武器(self)并没有到达它应该到的位置，
-		# 则认为该武器(self)仍在左右换边过程中，不去动on_changing_side.x
-		# 否则，则认为该武器(self)的左右换边过程已经结束，设置on_changing_side_x为false
-		if weapon_offset_from_chara.x > (target_offset.x - 0.11) :
-			on_changing_side.x = false
-		# 如果鼠标、该武器(self)正好在角色所在位置的左右两边，说明要换方向了，设置on_changing_side.x为true
-		elif weapon_offset_from_chara.x < 0 :
-			on_changing_side.x = true
-	else :
-		scale.x = -1
-		target_offset = Vector2(-15, 0)
-		
-		# 同上
-		if weapon_offset_from_chara.x < (target_offset.x + 0.11) :
-			on_changing_side.x = false
-		# 同上
-		elif weapon_offset_from_chara.x > 0 :
-			on_changing_side.x = true
-	
-	# 线性插值的时候的取值间隔
-	var linear_interpolate_scale_rate : = Vector2(0.2, 0.1)
-	
-	# 如果追随角色的该武器(self)正在左右换边，设置一下非线性动画平滑
-	if on_changing_side.x :
-		linear_interpolate_scale_rate.x = 0.3
-		
-		# 追随角色的该武器(self)正在左右换边时，如果鼠标和角色运动方向同向，
-		# 为了防止追不上角色，把linear_interpolate_scale_rate.x调大
-		if (owner.owner.input_module.get_direction().x > 0 && mouse_offset_from_chara.x > 0) || (owner.owner.input_module.get_direction().x < 0 && mouse_offset_from_chara.x < 0) :
-			linear_interpolate_scale_rate.x = 0.5
-		
-	# 如果追随角色的该武器(self)没有在左右换边的过程中，为了防止追不上角色，
-	# 把linear_interpolate_scale_rate.x调大
-	else :
-		linear_interpolate_scale_rate.x = 0.5
-	
-	# 水平方向非线性动画
-	global_position = global_position.linear_interpolate(Vector2(owner.owner.character.global_position.x + target_offset.x, self.global_position.y), linear_interpolate_scale_rate.x)
-	# 垂直方向非线性动画
-	global_position = global_position.linear_interpolate(Vector2(global_position.x, owner.owner.character.global_position.y + target_offset.y + mouse_offset_from_chara.y/5), linear_interpolate_scale_rate.y)
--- Actors/Player/Weapon/Weapon.gd
+++ Actors/Player/Weapon/Weapon.gd
@@ -3,7 +3,99 @@ extends Node2D
 class_name Weapon
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	owner=get_parent()
 	pass # Replace with function body.
 
+# 追随角色的该武器(self)是否正在换边过程中
+var on_changing_side : = Vector2(false, false)
+
 func follow_player():
-	pass
+	# 该武器(self)位置和角色位置的固定偏移量，其中fix_offset.x为绝对值
+	var fix_offset :=Vector2(13, -10)
+	# 该武器(self)位置和角色位置的目标偏移量
+	var target_offset : = Vector2(0, 0)
+	# 当前鼠标位置和角色位置的偏移量
+	var mouse_offset_from_chara : = Vector2(owner.input_module.mouse_global_position -  owner.character.global_position)
+	# 当前该武器(self)位置和角色位置的偏移量
+	var weapon_offset_from_chara : = Vector2(global_position - owner.character.global_position)
+	
+	# 武器y坐标绝对值的最大值（不包括固定偏移量）
+	var max_abs_y : = 16
+	# 操控武器y坐标（不包括固定偏移量）的鼠标y坐标绝对值 和 武器y坐标（不包括固定偏移量）的比值
+	# 用人话来说，就是鼠标操控范围和武器移动范围的比值，或者称之为放大倍数
+	var control_y_ratio : = 2
+	
+	# 设定target_offset.y
+	# 先判断鼠标在角色的上方还是下方，分别处理
+	if(mouse_offset_from_chara.y > 0) :
+		# 如果鼠标移动到超过鼠标移动范围的区域，则直接设置武器y坐标的绝对值为其最大值（然后还得加上固定偏移量）
+		if mouse_offset_from_chara.y > max_abs_y * control_y_ratio :
+			target_offset.y = fix_offset.y + max_abs_y
+		# 如果鼠标的y坐标偏移量没超过范围，则设置武器y坐标的绝对值为 鼠标的y坐标偏移量/放大倍数（然后还得加上固定偏移量）
+		else :
+			target_offset.y = fix_offset.y + mouse_offset_from_chara.y / 2
+	else :
+		# 同上
+		if mouse_offset_from_chara.y < - max_abs_y * control_y_ratio :
+			target_offset.y = fix_offset.y - max_abs_y
+		# 同上
+		else :
+			target_offset.y = fix_offset.y + mouse_offset_from_chara.y / 2
+	
+	# 弧线的参数，为椭圆，满足y=asinθ，x=bcosθ（y需要先减去固定偏移量）
+	# 注意，ellipse_a不能小于max_abs_y
+	var ellipse_a = max_abs_y
+	var ellipse_b = ellipse_a / 4
+	# θ=arcsin(y/a)（y需要先减去固定偏移量）
+	var ellipse_theta = asin((target_offset.y - fix_offset.y) / ellipse_a)
+	
+	# 设定target_offset.x
+	# 先判断鼠标在角色的左边还是右边，分别处理
+	if(mouse_offset_from_chara.x > 0) :
+		scale.x = 1
+		fix_offset.x = +fix_offset.x # 向右偏移
+		# x=bcosθ（x需要再加上固定偏移量）
+		target_offset.x = fix_offset.x + ellipse_b * cos(ellipse_theta)
+		
+		# 如果鼠标、该武器(self)并不在角色所在位置的左右两边，但是该武器(self)并没有到达它应该到的位置，
+		# 则认为该武器(self)仍在左右换边过程中，不去动on_changing_side.x
+		# 否则，则认为该武器(self)的左右换边过程已经结束，设置on_changing_side_x为false
+		if weapon_offset_from_chara.x > (target_offset.x - 0.11) :
+			on_changing_side.x = false
+		# 如果鼠标、该武器(self)正好在角色所在位置的左右两边，说明要换方向了，设置on_changing_side.x为true
+		elif weapon_offset_from_chara.x < 0 :
+			on_changing_side.x = true
+	else :
+		scale.x = -1
+		fix_offset.x = -fix_offset.x # 向左偏移
+		# x=bcosθ（x需要再加上固定偏移量）
+		target_offset.x = fix_offset.x - ellipse_b * cos(ellipse_theta)
+		
+		# 同上
+		if weapon_offset_from_chara.x < (target_offset.x + 0.11) :
+			on_changing_side.x = false
+		# 同上
+		elif weapon_offset_from_chara.x > 0 :
+			on_changing_side.x = true
+	
+	# 线性插值的时候的取值间隔
+	var linear_interpolate_scale_rate : = Vector2(0.2, 0.35)
+	
+	# 如果追随角色的该武器(self)正在左右换边，设置一下非线性动画平滑
+	if on_changing_side.x :
+		linear_interpolate_scale_rate.x = 0.25
+		
+		# 追随角色的该武器(self)正在左右换边时，如果鼠标和角色运动方向同向，
+		# 为了防止追不上角色，把linear_interpolate_scale_rate.x调大
+		if (owner.input_module.get_direction().x > 0 && mouse_offset_from_chara.x > 0) || (owner.input_module.get_direction().x < 0 && mouse_offset_from_chara.x < 0) :
+			linear_interpolate_scale_rate.x = 0.5
+		
+	# 如果追随角色的该武器(self)没有在左右换边的过程中，为了防止追不上角色，
+	# 把linear_interpolate_scale_rate.x调大
+	else :
+		linear_interpolate_scale_rate.x = 0.5
+	
+	# 水平方向非线性动画
+	global_position = global_position.linear_interpolate(Vector2(owner.character.global_position.x + target_offset.x, self.global_position.y), linear_interpolate_scale_rate.x)
+	# 垂直方向非线性动画
+	global_position = global_position.linear_interpolate(Vector2(global_position.x, owner.character.global_position.y + target_offset.y), linear_interpolate_scale_rate.y)
