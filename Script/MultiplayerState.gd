ab8406a4395ee114949b6ead6cee0b728b28d778
DotTorrent <pointtorrent@outlook.com>
2021-11-05 17:12:08 +0800

    多人联机入口关卡和多人游戏初始化实现

--- Script/MultiplayerState.gd
+++ Script/MultiplayerState.gd
@@ -9,31 +9,36 @@ class PlayerInfo:
 	var type: String = "" # fire or ice
 
 # 基本属性：联网id，名字，类型
-var myId : int
+var myId : int = 0
 var myInfo: PlayerInfo
-var remotePlayerId : int
+var remotePlayerId : int = 0
 var remotePlayerInfo: PlayerInfo
+
+var myPlayerInstance: Node2D
+var remotePlayerInstance: Node2D
+
+var isMyPlayerDone:bool = false
+var isRemotePlayerDone:bool = false
+
 
 # 这里5个信号都是 Godot High-level multiplayer API 自带信号
 func _ready() -> void:
-	#var peer = NetworkedMultiplayerENet.new()
-	#peer.create_server(PORT, MAX_PLAYERS)
-	#get_tree().network_peer = peer
 	self.get_tree().connect('network_peer_connected', self, '_on_network_peer_connected')
 	self.get_tree().connect('network_peer_disconnected', self, '_on_network_peer_disconnected')
 	self.get_tree().connect('server_disconnected', self, '_on_server_disconnected')
 	self.get_tree().connect('connected_to_server', self, '_on_connected_to_server')
 	self.get_tree().connect('connection_failed', self, '_on_connection_failed')
 
-# 每当有终端连接到自己，就会调用该方法，不论自身是服务端还是客户端
+# 每当和终端连接成功（无论谁是发起方），就会调用该方法，不论自身是服务端还是客户端
 func _on_network_peer_connected(remote_id : int) -> void:
+	print_debug(remote_id, ' connected')
 	
-	# 通过 rpc_id 将自己的信息远程发送给对方
-	self.rpc_id(remote_id, 'setRemotePlayerInfo', myInfo)
+	# 通过 rpc_id 将自己的信息远程发送给对方进行注册
+	rpc_id(remote_id, 'registerPlayerInfo', myInfo)
 	
-	# 如果自身是服务端，则处理游戏准备事件
 	if self.get_tree().is_network_server():
-		print_debug(remote_id, 'connected')
+		self.get_tree().refuse_new_network_connections = true
+		
 
 # 每当有终端断开链接，就会调用该方法，不论自身是服务端还是客户端
 func _on_network_peer_disconnected(remote_id : int) -> void:
@@ -43,7 +48,7 @@ func _on_network_peer_disconnected(remote_id : int) -> void:
 func _on_connected_to_server() -> void:
 	pass
 
-# 与服务器断开连接(被t了)，仅当自身是客户端时调用
+# 与服务器断开连接(一般是被t了)，仅当自身是客户端时调用
 func _on_server_disconnected() -> void:
 	pass
 
@@ -56,15 +61,20 @@ func _on_connection_faileded() -> void:
 remote func registerPlayerInfo(remote_info: PlayerInfo) -> bool:
 	var remote_id = self.get_tree().get_rpc_sender_id()
 	# 如果我方没被连接过，则注册新玩家id和信息
-	if(remotePlayerId == null):
+	if(remotePlayerId == 0):
 		remotePlayerId = remote_id
 		remotePlayerInfo = remote_info
-	# 如果是同一id，则说明不是注册信息而是更新信息，只设置info
-	elif(remotePlayerId == remote_id):
+		pre_configure_game('res://Levels/InitialLevel.tscn')
+		return true
+	return false
+
+remote func updatePlayerInfo(remote_info: PlayerInfo) -> bool:
+	var remote_id = self.get_tree().get_rpc_sender_id()
+	# 如果本地储存的远程id和收到的远程id是同一id，则可以更新信息，设置info
+	if(remotePlayerId == remote_id):
 		remotePlayerInfo = remote_info
-	else:
-		return false
-	return true
+		return true
+	return false
 
 # 创建服务器，这里返回一个结果
 # 如果一个 IP 被占用就会返回错误
@@ -99,12 +109,68 @@ func joinGame(address: String, port:int, myName: String) -> bool:
 	
 	return true
 
-# 重设网络为 null ，断开所有连接
+# 重设网络为null，重设各个变量，断开所有连接
 func resetNetwork() -> void:
 	myId = 0
 	myInfo = PlayerInfo.new()
+	isMyPlayerDone = false
+	
 	remotePlayerId = 0
 	remotePlayerInfo = PlayerInfo.new()
+	isRemotePlayerDone = false
+	
 	if(self.get_tree().network_peer != null):
 		self.get_tree().network_peer.close_connection()
 	self.get_tree().network_peer = null
+
+# 初始化游戏关卡设置，加载好人物和关卡并设置主从关系
+func pre_configure_game(level: String):
+	# Load world
+	var world = load(level).instance()
+	get_node("/root").add_child(world)
+
+	# Load my player
+	myPlayerInstance = preload('res://Actors/Player/Player.tscn').instance()
+	myPlayerInstance.set_name(str(myId))
+	myPlayerInstance.set_network_master(myId)
+	world.add_child(myPlayerInstance)
+
+	# Load remote player
+	remotePlayerInstance = preload('res://Actors/Player/Player.tscn').instance()
+	remotePlayerInstance.set_name(str(remotePlayerId))
+	remotePlayerInstance.set_network_master(remotePlayerId)
+	world.add_child(remotePlayerInstance)
+	
+	# 先暂停
+	get_tree().set_pause(true)
+	
+	# 向服务端告知自身的游戏关卡设置初始化进度已经完成
+	# 自己是服务端的，也要向自身告知
+	rpc_id(1, 'set_pre_configure_done')
+
+# only execute when self(receiver) is server
+# 远程调用位于服务端的此函数，用于向服务端告知自身的游戏关卡设置初始化进度已经完成，
+# 服务端收到后设置对应的player的完成进度为true
+master func set_pre_configure_done():
+	# 只有服务端才接受该调用
+	if not get_tree().is_network_server():
+		return
+	
+	if get_tree().get_rpc_sender_id() == myId:
+		isMyPlayerDone = true
+		print(myId, ' done')
+	elif get_tree().get_rpc_sender_id() == remotePlayerId:
+		isRemotePlayerDone = true
+		print(remotePlayerId, ' done')
+	
+	# 如果服务端自身和远程客户端的游戏关卡设置初始化都完成，则让所有master节点开始游戏
+	if isMyPlayerDone and isRemotePlayerDone:
+		rpc('post_configure_game') # sent to all master
+
+# only execute when remote(sender) is server
+# 所有节点的游戏关卡设置都初始化完成，游戏开始
+master func post_configure_game():
+	# 只接收服务端发送的信息
+	if 1 == get_tree().get_rpc_sender_id():
+		get_tree().set_pause(false)
+		print('start game')
